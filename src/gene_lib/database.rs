c6e1f0fa847ed5c93c2afff72930596c6fd04115
captaindrewsa <captaindrewsa@gmai.com>
2022-07-06 13:52:35 +0200

    Копания в сторону поиска реакций 0.1

--- src/gene_lib/database.rs
+++ src/gene_lib/database.rs
@@ -41,77 +41,34 @@ pub fn create_reactions_tree(compound: &str) -> Result<()> {
     let mut file_tree = File::create(path_to_tree)?;
     let mut file_reactions = get_reactions_db().unwrap();
     /*
-    let target : Vec<Vec<String>>; <-Compound в начале
-    let bufer : Vec<Vec<String>>;
-    for (eznym, reaction) in reactions{
-        if reaction.product.iter().all(|prod| {
-            if target.iter().all(|comps|{
-                if comps.contains(prod){
-
-                }
-            })
-        }){
-            todo!()
-        }
-    }
     */
 
     Ok(())
 }
 
-// #[derive(Debug)]
-// struct Enzyme{
-//     enzyme_code: String,
-//     Name: String,
-//     Subsrat: String,
-//     Product: String
-// }
+/*
+fn find_reactions_for_buffer(substrat: Vec<String>) -> Vec<String>{
+    
+    let mut output_modules: Vec<Vec<String>> = Vec::new();
 
-// pub fn test_fn() -> Result<mysql::PooledConn>{
-//     let url = "mysql://root:Asavoz76Vlas5712Drewsa_Bad@localhost:3306/enzyme";
-//     // let mut builder = OptsBuilder::new();
-//     // builder.ip_or_hostname(Some("root@localhost"))
-//     //     .db_name(Some("enzyme"))
-//     //     .ssl_opts(Some(SslOpts::default()));
+    loop{
+        let mut buffer = HashSet::from(substrat);
+        let mut tmp_reactions_module: Vec<String> = Vec::new();
 
-//     let pool = Pool::new(Opts::from_url(url)?).expect("Не приконектилось");
-//     let mut conn = pool.get_conn()?;
+        for reaction in reactions.db{
+            let reactoion_buffer = HashSet::from(reaction.product);
+            if buffer.difference(&reaction_buffer) != buffer{
+                buffer = buffer.difference(&reaction_buffer);
+                tmp_reactions_module.push(reaction.name);
+                continue;
+            } else if buffer.is_empty() && !tmp_reaction_module.is_empty(){
+                output_modules.push(tmp_reaction_module);
+                /*КАК_ТО СДЕЛАТЬ ЧЕРНЫЙ ЛИСТ РЕАКЦИЙ. ПРОВЕРКА НА ИЗМЕНЕНИЯ С ПРЕДЫДУЩИМ ЦИКЛОМ */
+                break;
+            }
+        }
 
-//     conn.query_drop(
-//         "USE enzyme;"
-//     )?;
+    }
+}
+*/
 
-//     conn.query_drop(
-//         r"CREATE TABLE IF NOT EXIST enzymes (
-//             EnzymeCode CHAR(50) not null,
-//             Name CHAR(255),
-//             Substrate CHAR(500),
-//             Product CHAR(500)
-//         )"
-//     )?;
-
-//     let enzyme_list = vec![
-//         Enzyme{ enzyme_code: "EC 3.1.3.48".to_string(), Name: "PTPase".to_string(), Subsrat: "C01167_IN;C00001_IN".to_string(), Product: "C00585_IN;C00009_IN".to_string()}
-//     ];
-
-//     conn.exec_batch(
-//         r"INSERT INTO enzymes (Enzyme_code, Name, Substrate, Product) VALUES(:enzyme_code, :name, :sub, :prod)"
-//         , enzyme_list
-//             .iter()
-//             .map(|e| params! {
-//                 "enzyme_code"=> e.enzyme_code.clone(),
-//                 "name" => e.Name.clone(),
-//                 "sub"=> e.Subsrat.clone(),
-//                 "prod" => e.Product.clone()
-//             })
-//         )?;
-//     let selected_enzyme = conn
-//         .query_map(
-//             "SELECT * FROM enzymes",
-//             |(code, name, sub, prod)|{
-//                 Enzyme{ enzyme_code: code, Name: name, Subsrat: sub, Product: prod}
-//             },
-//             )?;
-//     println!("{:?}", selected_enzyme[0]);
-//     Ok(conn)
-// }
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@ mod gene_lib;
 use diplom::gene_lib::{compartment, database, main_system::GeneSys};
 
 fn main() {
-    let mut sys = GeneSys::default();
-    sys.create_compartments(vec!["Клетка","Среда"]);
-    sys.create_tree(vec![
-        ("Среда", vec!["Клетка"])
-    ]);
-    sys.add_substance_to_compartment("Клетка", "EC 3.1.3.48");
-    sys.add_substance_to_compartment("Клетка", "C01167");
-    sys.add_substance_to_compartment("Клетка", "C00001");
-    sys.compile();
-    sys.print_result();
+    // let mut sys = GeneSys::default();
+    // sys.create_compartments(vec!["Клетка","Среда"]);
+    // sys.create_tree(vec![
+    //     ("Среда", vec!["Клетка"])
+    // ]);
+    // sys.add_substance_to_compartment("Клетка", "EC 3.1.3.48");
+    // sys.add_substance_to_compartment("Клетка", "C01167");
+    // sys.add_substance_to_compartment("Клетка", "C00001");
+    // sys.compile();
+    // sys.print_result();
+    
 }
