6fe8a1ac0b1625655fcd28a99fa7152b40b71ef7
captaindrewsa <captaindrewsa@gmai.com>
2022-07-09 23:06:49 +0200

    Работает create_reactions_tree

--- src/gene_lib/database.rs
+++ src/gene_lib/database.rs
@@ -5,7 +5,7 @@
 use std::{
     collections::{HashMap, HashSet, BTreeMap},
     fs::{self, File},
-    io::{Read, Result}, vec,
+    io::{Read, Result}, vec, process::Output,
 };
 
 pub fn get_reactions_db() -> Result<BTreeMap<String, Vec<Vec<String>>>> {
@@ -36,29 +36,44 @@ pub fn get_reactions_db() -> Result<BTreeMap<String, Vec<Vec<String>>>> {
     Ok(otp) //Возвращаем словарь с Энзим - Субстрт/Продукт
 }
 
-pub fn create_reactions_tree(compound: &str) -> Result<()> {
-    // let path_to_tree = "db/reactions_tree.csv";
-    // let mut file_tree = File::create(path_to_tree)?;
-    // let mut file_reactions = get_reactions_db().unwrap();
+pub fn create_reactions_tree_light(compounds: Vec<&str>) -> Option<Vec<String>> {
+    let mut output_reactions: Vec<String> = Vec::new();
+    let mut reaction_buff: String = String::new();
+    let mut comp_buff: Vec<String> = add_three_symbols(compounds);
+    
+    loop {
+        if let Some(reactions) = find_reactions_for_buffer(comp_buff.clone()){
+            output_reactions.push(reactions.clone());
+            reaction_buff = reactions.clone();
+            // println!("{}",reactions.clone());
+        } else {
+            break;
+        }
+        if let Some(substrats) = get_substrat_from_reactions(reaction_buff.clone()){
+            comp_buff = substrats.clone().iter().map(|s| s.to_string()).collect();
+        } else { 
+            break;
+        }
 
-
-
-    Ok(())
+    }
+    if !output_reactions.is_empty(){
+        Some(vec![output_reactions.join("<-"), comp_buff.join(";")])
+    } else {
+        None
+    }
+    
 }
 
 
-pub fn find_reactions_for_buffer(substrat: Vec<&str>) -> String{    
+fn find_reactions_for_buffer(substrat: Vec<String>) -> Option<String>{    
     let mut output_modules: Vec<String> = Vec::new();
-    // let mut output_check = &output_modules.clone();
-    
     loop{
         
         let mut tmp_reaction_buffer: Vec<String> = Vec::new(); 
-        let mut sub_buff = add_vec_to_set(substrat.clone().iter().map(|elem| {elem.to_string()}).collect());
+        let mut sub_buff = add_vec_to_set(substrat.clone().iter().map(|elem| {elem[0..elem.len()-3].to_string()}).collect());
         let mut black_reaction_list: HashSet<String> = HashSet::new();
 
         for (reaction_name, substrat_product) in get_reactions_db().unwrap().iter(){
-            // println!("ОТЛАДОЧНАЯ ИНФОРМАЦИЯ: {}|{:?}", &reaction_name, &substrat_product);
             let products: HashSet<String>= add_vec_to_set(substrat_product[1].clone().iter().map(|elem| {elem.to_owned()[..elem.len()-3].to_string()}).collect());
             let diff = HashSet::from_iter(sub_buff.difference(&products).map(|elem| {elem.clone()}));
             if diff != sub_buff && !black_reaction_list.contains(reaction_name){
@@ -78,8 +93,32 @@ pub fn find_reactions_for_buffer(substrat: Vec<&str>) -> String{
         break;
        
     }
-    let output = output_modules.join("*");
-    return output;
+    
+    if !output_modules.is_empty(){
+        let output = output_modules.join(";");
+        Some(output)
+    } else {
+        None
+    }
+}
+
+fn get_substrat_from_reactions(module: String)-> Option<Vec<String>>{
+    let reacions: Vec<String> = module.split(";").map(|elem| {elem.to_string()}).collect();
+    let mut out: Vec<String> = Vec::new();
+    for reaction in reacions.iter(){
+        if let Some(sub_prod) = get_reactions_db().unwrap().get(reaction){
+            let mut tmp = sub_prod.clone();
+            out.append(&mut tmp[0]);
+        } else {
+            continue;
+        }
+    }
+    if !out.is_empty(){
+        Some(out)
+    } else {
+        None
+    }
+
 }
 
 fn add_vec_to_set(sub: Vec<String>)-> HashSet<String>{
@@ -90,6 +129,17 @@ fn add_vec_to_set(sub: Vec<String>)-> HashSet<String>{
         set.insert(elem);
     }
     return set;
+}
+
+fn add_three_symbols(stroka: Vec<&str>)-> Vec<String>{
+    let tmp: Vec<String> = stroka.iter().map(|s| {s.to_string()}).collect();
+    let mut output_stroka: Vec<String>= Vec::new();
+    for elem in tmp{
+        let mut elem_tmp = elem.clone();
+        elem_tmp.push_str("000");
+        output_stroka.push(elem_tmp);
+    }
+    return output_stroka;
 }
 
 
